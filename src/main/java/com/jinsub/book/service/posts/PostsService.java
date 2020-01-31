263a14e9fdd18b6798055d07502c833b756daa1d
Jinsub Kim <less8430@naver.com>
2020-01-31 21:33:10 +0900

    글 전체 조회 기능 추가

--- src/main/java/com/jinsub/book/service/posts/PostsService.java
+++ src/main/java/com/jinsub/book/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jinsub.book.service.posts;
 
 import com.jinsub.book.domain.posts.Posts;
 import com.jinsub.book.domain.posts.PostsRepository;
+import com.jinsub.book.web.dto.PostsListResponseDto;
 import com.jinsub.book.web.dto.PostsResponseDto;
 import com.jinsub.book.web.dto.PostsSaveRequestDto;
 import com.jinsub.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
